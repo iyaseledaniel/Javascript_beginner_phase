137b31fa24087d4403459ea4e7e36192fc9e56ca
iyaseledaniel <iyaselealfred007@gmail.com>
2026-05-09 16:07:05 -0500

    tested and added ip rate limiting

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const {getAllQuotes, getRandomQuotes, getCategoryQuotes} = require('./controller/quotesController')
 
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 mins
+  max: 10 // limit each IP
+});
+
 const port = 4040;
 const app = express()
 
 app.use(express.json());
+app.use(limiter);
 
 app.get('/', (req, res) => {
     res.send("<h1>Welcome to Quotes world </h1>")
